c1dded51caf2dbfea8a4294daf95bbade2b583fb
Orozco Marianela <orozcomarianela10@gmail.com>
2022-10-29 23:42:26 -0300

    Marianela Orozco
    Ciclos ejercicio 11
    Asistencia Octubre

--- Asistencia, ejercicio caja.java
+++ Asistencia, ejercicio caja.java
@@ -90,3 +90,21 @@ public class Aritmetica {
             return this.sumarConRetorno();
        }   
 }
+
+// Marianela Orozco
+// Ejercicio 11: Diseñar un programa que muestre el producto
+// de los 10 primeros numeros impares
+// Hacerlo con JOptionPane
+package ciclos11;
+
+import javax.swing.JOptionPane;
+
+public class Ciclos11 {
+    public static void main(String[] args) {
+        long producto = 1;
+        for(int i = 1; i<=20;i+=2){ 
+            producto *= i;
+        }
+        JOptionPane.showMessageDialog(null, "El producto de los números impares es: "+producto);
+    }
+}
