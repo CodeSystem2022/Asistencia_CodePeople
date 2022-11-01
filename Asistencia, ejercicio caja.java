f1c5e7fde2ae20ef2802ae4d24ddb97cebe047ed
Daniela Armijo <111472176+Dany1970@users.noreply.github.com>
2022-10-31 22:47:55 -0300

    Update Asistencia, ejercicio caja.java
    31/10 Agregado un ejercicio correspondiente al mes de octubre

--- Asistencia, ejercicio caja.java
+++ Asistencia, ejercicio caja.java
@@ -1,3 +1,39 @@
+//Daniela Armijo
+//Ejercicio Ciclos 11 - Clase 7 - 14 de octubre
+import javax.swing.JOptionPane;
+
+/*
+Ejercicio 11: Diseñar un programa que muestre el producto de los 10 primeros 
+números impares.
+Hacerlo con JOptionpane
+*/
+public class Ciclos11 {//Estoy tratando de memorizar los pasos, así es que voy 
+                        // a documentar paso a paso
+    //Lo primero es llamar al método main
+    public static void main(String[] args) {
+        //Como vamos a usar JOptionPane, definimos las variables. En principio
+        //se necesitan tres: número, producto y un contador, todos de clase entero
+        //El int se coloca una sola vez. Se inicializan las variables:
+        int numero = 1, producto = 1, contador = 0;
+        //Imprimimos una línea que explicará qué hace el código con una ventana
+        JOptionPane.showMessageDialog(null, "El siguiente programa calculará el producto de los "
+                + "diez primeros números impares ");
+        
+        //Hacemos un bucle con do-while para que el código repita la operación hasta
+        //que el contador llegue a un valor menor a 10.
+        do{
+            producto *= numero;
+            contador +=1;
+            numero +=2;
+            
+            }while(contador <10);
+        
+//La ventana de mensaje mostrará el resultado de la operación:        
+        JOptionPane.showMessageDialog(null, "el producto de los diez primeros números "
+                + "impares es: "+producto);
+                }
+        }
+
 //Nicolas Calvo
 
 package caja;
@@ -46,24 +82,7 @@ public int calcularVolumen () {
         
     }
 }
-   //Daniela Armijo
-    package Operaciones;
-
-public class PruebaAritmetica {
-    public static void main(String[] args) {
-        Aritmetica aritmetica1 = new Aritmetica();
-        aritmetica1.a = 3;
-        aritmetica1.b = 1;
-        aritmetica1.sumarNumeros();
-        
-        int resultado = aritmetica1.sumarConRetorno();
-        System.out.println("resultado = " + resultado);
-        
-        resultado = aritmetica1.sumarConArgumentos(12, 26);
-        System.out.println("resultado usando argumentos = "+resultado);
-    }
-    
-}
+   
 //Beatriz Ramírez
 package Operaciones;
 
