e820ae36af005210b234b1a58e48495f66422fd7
Matias Kudaka <matiaskudaka@gmail.com>
2022-11-07 12:17:24 -0300

    Update Asistencia, ejercicio ciclos 10
    Asistencia Matias Kudaka

--- Asistencia, ejercicio caja.java
+++ Asistencia, ejercicio caja.java
@@ -130,3 +130,28 @@ public class Ciclos11 {
         JOptionPane.showMessageDialog(null, "El producto de los números impares es: "+producto);
     }
 }
+
+// Matias Kudaka
+//Ejercicio 10: Pedir 10 umero y escribir la suma total
+//Hacerlo con la clase Scanner y JOptionPane
+
+package ciclos10;
+
+import java.util.Scanner;
+
+
+public class Ciclos10 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero, suma = 0;
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Digite un numero: ");
+            numero = Integer.parseInt(entrada.nextLine());
+            suma += numero;
+        }
+       
+        System.out.println("La suma de todos los numeros es: " + suma);
+        
+    }
+    
+}
